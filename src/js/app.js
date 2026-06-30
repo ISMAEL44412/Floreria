8d53af0babb3b8b14e702cc088bc50962e17e4d1
ISMAEL44412 <rig8528@gmail.com>
2026-06-29 23:43:44 -0300

    fixeds

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,14 @@
 const imagenesCatalogo = document.querySelectorAll('.catalogo__producto__imagen');
 
 console.log(imagenesCatalogo)
+
+imagenesCatalogo.forEach(imagen => {
+    imagen.addEventListener("dblclick", (evento)=> {
+        
+        //* Agrega la clase imagenSeleccionada a la imagen donde se dio doble click.
+        evento.target.classList.add('imageneSeleccionada'); 
+
+        // ? Ver como agregar o eliminar la clase cuando se selecciona otra, agregar modal
+        console.log(evento.target)
+    })
+})
