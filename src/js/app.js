cbfbcc5eb6d0cc449d60ce5f3c5a9008f155cb09
Ismael <rig8528@gmail.com>
2026-07-13 23:54:41 -0300

    alerta de mensaje cuando no se ingresan los datos

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,34 @@
-const submit = document.querySelector('form');
-submit.addEventListener('submit', function(e){
+const submit = document.querySelector("form");
+submit.addEventListener("submit", function (e) {
+  let usuario = document.getElementById("email").value.trim();
+  let contraseña = document.getElementById("password").value.trim();
 
-  let usuario = document.getElementById('email').value.trim();
-  let contraseña = document.getElementById('password').value.trim();
+  e.preventDefault();
 
-  // e.preventDefault();
-  if(usuario== '' | contraseña == '') {
-    console.log("Usuario y contraseña necesarios");
-  } else {
-    console.log("Paso la verificacion");
+  let mensaje = "";
+  if ((usuario == "") || (contraseña == "")) {
+    mensaje = "Usuario o contraseña necesarios";
   }
 
-})
+  if (mensaje !== "") {
+    const formulario = document.querySelector('.formulario');
+    const botonSubmit = document.querySelector('.formulario__submit');
+
+    const divMensaje = document.createElement('DIV');
+    const parrafo = document.createElement('P');
+    parrafo.textContent = mensaje;
+
+    divMensaje.classList.add('mensaje')
+
+    divMensaje.appendChild(parrafo);
+    botonSubmit.before(divMensaje);
+
+    setTimeout(() => {
+      divMensaje.classList.add('oculto');
+    }, 2000);
+
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 5000);
+  }
+});
