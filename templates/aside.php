b4f3b7ef74bbeeaf3b57cdfd91f58646a64265ae
Ismael <rig8528@gmail.com>
2026-07-28 00:05:19 -0300

    new files

---
+++ templates/aside.php
@@ -0,0 +1,12 @@
+<aside class="aside">
+    <div class="aside__logo">
+        <img loading="lazy" class="navegacion__imagen" src="../img_webp/logo_sin_background.webp" alt="">
+    </div>
+    <nav class="aside__navegacion">
+        <a href="../index.php">Ir a la pagina</a>
+        <a href="/admin/dashboard.php">Dashboard</a>
+        <a href="/admin/productos.php">Productos</a>
+        <a href="/admin/configuracion.php">Configuración</a>
+        <a href="/logout.php" class="cerrar_sesion">Cerrar Sesión</a>
+    </nav>
+</aside>
