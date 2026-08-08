bf03fb15c751cbd0eacb4489dbbc0a5e469fdd62
Ismael <rig8528@gmail.com>
2026-08-08 00:29:16 -0300

    fixeds

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,8 +3,19 @@ require_once __DIR__ . '/../includes/funciones.php';
 require_once __DIR__ . '/../includes/config/database.php';
 
 
+
 isAuth();
-$titulo ="Dashboard";
+$db = database();
+
+$titulo = "Productos";
+
+$allProducts = "SELECT * FROM productos";
+
+$productos = [];
+$resultado = mysqli_query($db, $allProducts);
+while ($producto = $resultado->fetch_object()) {
+    $productos[] = $producto;
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -25,5 +36,32 @@ $titulo ="Dashboard";
 </head>
 
 <body class="dashboard">
-    <?php include __DIR__ ."/../templates/aside.php"; ?>
+    <?php include __DIR__ . "/../templates/aside.php"; ?>
+    <main class="dashboard__main">
+        <h1 class="dashboard__titulo"><?php echo $titulo ?></h1>
+        <a href="#" class="dashboard__crear">Crear nuevo producto</a>
+        <table class="tabla">
+            <thead class="tabla__head">
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Nombre del producto</th>
+                    <th scope="col">Cantidad de flores</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody class="tabla__body">
+                <?php foreach ($productos as $producto): ?>
+                    <tr>
+                        <td><?php echo $producto->id ?></td>
+                        <td><?php echo $producto->nombre ?></td>
+                        <td><?php echo $producto->cantidad_flores ?></td>
+                        <td>
+                            <a href="#">Eliminar</a>
+                            <a href="#">Editar</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </main>
 </body>
--- templates/aside.php
+++ templates/aside.php
@@ -5,7 +5,6 @@
     <nav class="aside__navegacion">
         <a href="../index.php">Ir a la pagina</a>
         <a href="/admin/dashboard.php">Dashboard</a>
-        <a href="/admin/productos.php">Productos</a>
         <a href="/admin/configuracion.php">Configuración</a>
         <a href="/logout.php" class="cerrar_sesion">Cerrar Sesión</a>
     </nav>
