0836a0089a5bb60fbf508365131f3b02e42b2ab0
Ismael <rig8528@gmail.com>
2026-08-12 20:46:36 -0300

    fixeds

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -25,7 +25,7 @@ while ($producto = $resultado->fetch_object()) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Flores Eternas</title>
     <link rel="stylesheet" href="../build/css/app.css">
-    <link rel="shortcut icon" href="img_webp/flor.webp" type="image/x-icon">
+    <link rel="shortcut icon" href="../img_webp/flor.webp" type="image/x-icon">
     <link rel="preload" href="../build/css/app.css" as="style" />
     <!-- * Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -40,28 +40,30 @@ while ($producto = $resultado->fetch_object()) {
     <main class="dashboard__main">
         <h1 class="dashboard__titulo"><?php echo $titulo ?></h1>
         <a href="#" class="dashboard__crear">Crear nuevo producto</a>
-        <table class="tabla">
-            <thead class="tabla__head">
-                <tr>
-                    <th scope="col">id</th>
-                    <th scope="col">Nombre del producto</th>
-                    <th scope="col">Cantidad de flores</th>
-                    <th scope="col">Acciones</th>
-                </tr>
-            </thead>
-            <tbody class="tabla__body">
-                <?php foreach ($productos as $producto): ?>
+        <div class="contenedor__tabla">
+            <table class="tabla">
+                <thead class="tabla__head">
                     <tr>
-                        <td><?php echo $producto->id ?></td>
-                        <td><?php echo $producto->nombre ?></td>
-                        <td><?php echo $producto->cantidad_flores ?></td>
-                        <td>
-                            <a href="#">Eliminar</a>
-                            <a href="#">Editar</a>
-                        </td>
+                        <th scope="col">id</th>
+                        <th scope="col">Nombre del producto</th>
+                        <th scope="col">Cantidad de flores</th>
+                        <th scope="col">Acciones</th>
                     </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody class="tabla__body">
+                    <?php foreach ($productos as $producto): ?>
+                        <tr>
+                            <td><?php echo $producto->id ?></td>
+                            <td><?php echo $producto->nombre ?></td>
+                            <td><?php echo $producto->cantidad_flores ?></td>
+                            <td>
+                                <a href="#">Eliminar</a>
+                                <a href="#">Editar</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
     </main>
 </body>
